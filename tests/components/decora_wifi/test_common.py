3d0eab701c722594e9b92f04de88296f88c04350
TheBassEngineer <souletech@gmail.com>
2021-09-06 18:52:42 +0000

    Update tests for new session storage

--- tests/components/decora_wifi/test_common.py
+++ tests/components/decora_wifi/test_common.py
@@ -1,35 +1,42 @@
 """Test the decora_wifi Common components."""
 
+from datetime import timedelta
+import logging
 from unittest.mock import patch
 
 from homeassistant.components.decora_wifi.common import (
-    DecoraWifiCommFailed,
+    CommFailed,
     DecoraWifiEntity,
-    DecoraWifiLoginFailed,
     DecoraWifiPlatform,
+    LoginFailed,
 )
-from homeassistant.components.decora_wifi.const import LIGHT_DOMAIN
+from homeassistant.components.decora_wifi.const import DEFAULT_SCAN_INTERVAL, DOMAIN
+from homeassistant.helpers.entity_component import EntityComponent
 
 from tests.components.decora_wifi.common import (
+    SWITCH_NAME,
     FakeDecoraWiFiIotSwitch,
     FakeDecoraWiFiResidence,
     FakeDecoraWiFiResidentialAccount,
     FakeDecoraWiFiSession,
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 USERNAME = "username@home-assisant.com"
 PASSWORD = "test-password"
 INCORRECT_PASSWORD = "incoreect-password"
 
 
-def test_DecoraWifiPlatform_init():
+async def test_DecoraWifiPlatform_init(hass):
     """Check DecoraWifiPlatform initialization and deletion."""
     instance = None
+    component = EntityComponent(_LOGGER, DOMAIN, hass, timedelta(DEFAULT_SCAN_INTERVAL))
 
     with patch(
-        "homeassistant.components.decora_wifi.common.DecoraWiFiSession.login",
-        return_value=True,
-    ) as mock_session_login, patch(
+        "homeassistant.components.decora_wifi.common.DecoraWiFiSession",
+        side_effect=FakeDecoraWiFiSession,
+    ) as mock_session_construct, patch(
         "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_get_devices"
     ) as mock_apigetdevices, patch(
         "homeassistant.components.decora_wifi.common.Person.logout"
@@ -38,24 +45,33 @@ def test_DecoraWifiPlatform_init():
         instance = DecoraWifiPlatform(USERNAME, PASSWORD)
         instance.setup()
         assert isinstance(instance, DecoraWifiPlatform)
-        mock_session_login.assert_called_once()
+        mock_session_construct.assert_called_once()
         mock_apigetdevices.assert_called_once()
-        mock_session_login.reset_mock()
+        mock_session_construct.reset_mock()
         mock_apigetdevices.reset_mock()
+
+        # Check addition of object to hass as entity
+        await component.async_add_entities([instance], False)
+
         # Check object reauth
         instance.reauth()
         mock_person_logout.assert_called_once()
-        mock_session_login.assert_called_once()
+        mock_session_construct.assert_called_once()
         mock_person_logout.reset_mock()
+
         # Check object refresh devices
         instance.refresh_devices()
         mock_apigetdevices.assert_called_once()
-        # Check object teardown
-        instance.teardown()
+
+        # Check object teardown on removal from hass
+        entity = component.get_entity(instance.entity_id)
+        assert entity
+        platform = entity.platform
+        await platform.async_remove_entity(instance.entity_id)
         mock_person_logout.assert_called_once()
 
 
-def test_DecoraWifiPlatform_init_invalidpw():
+async def test_DecoraWifiPlatform_init_invalidpw(hass):
     """Check DecoraWifiPlatform login failure throws correct exception."""
     instance = None
     exception = None
@@ -66,7 +82,7 @@ def test_DecoraWifiPlatform_init_invalidpw():
     ) as mock_session_login, patch(
         "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_get_devices"
     ) as mock_apigetdevices, patch(
-        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform.api_logout"
+        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_logout"
     ) as mock_apilogout:
         # Check exception thrown as expected
         try:
@@ -74,13 +90,13 @@ def test_DecoraWifiPlatform_init_invalidpw():
             instance.setup()
         except Exception as ex:
             exception = ex
-        assert isinstance(exception, DecoraWifiLoginFailed)
+        assert isinstance(exception, LoginFailed)
         mock_session_login.assert_called_once()
         mock_apigetdevices.assert_not_called()
         mock_apilogout.assert_not_called()
 
 
-def test_DecoraWifiPlatform_init_nocomms():
+async def test_DecoraWifiPlatform_init_nocomms(hass):
     """Check DecoraWifiPlatform communication failure throws correct exception."""
     instance = None
     exception = None
@@ -91,7 +107,7 @@ def test_DecoraWifiPlatform_init_nocomms():
     ) as mock_session_login, patch(
         "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_get_devices"
     ) as mock_apigetdevices, patch(
-        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform.api_logout"
+        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_logout"
     ) as mock_apilogout:
         # Check exception thrown as expected
         try:
@@ -99,13 +115,13 @@ def test_DecoraWifiPlatform_init_nocomms():
             instance.setup()
         except Exception as ex:
             exception = ex
-        assert isinstance(exception, DecoraWifiCommFailed)
+        assert isinstance(exception, CommFailed)
         mock_session_login.assert_called_once()
         mock_apigetdevices.assert_not_called()
         mock_apilogout.assert_not_called()
 
 
-def test_DecoraWifiPlatform_getdevices():
+async def test_DecoraWifiPlatform_getdevices(hass):
     """Check DecoraWifiPlatform getdevices function."""
     instance = None
 
@@ -119,14 +135,16 @@ def test_DecoraWifiPlatform_getdevices():
         "homeassistant.components.decora_wifi.common.ResidentialAccount",
         side_effect=FakeDecoraWiFiResidentialAccount,
     ), patch(
-        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform.api_logout"
+        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_logout"
     ) as mock_apilogout:
         # Setup platform object
         instance = DecoraWifiPlatform(USERNAME, PASSWORD)
         instance.setup()
         assert isinstance(instance, DecoraWifiPlatform)
+
         # Check that getdevices left platform object in expected state
-        assert len(instance._iot_switches[LIGHT_DOMAIN]) == 6
+        assert len(instance.lights) == 6
+        assert len(instance.active_platforms) == 1
         # Check object teardown
         instance.teardown()
         mock_apilogout.assert_called_once()
@@ -143,10 +161,11 @@ async def test_async_setup_decora_wifi(hass):
         mock_platformsetup.assert_called_once()
 
 
-def test_DecoraWifiPlatform_apigetdevices_commfailed():
+async def test_DecoraWifiPlatform_apigetdevices_commfailed(hass):
     """Check DecoraWifiPlatform comm failure during initial getdevices."""
     instance = None
     exception = None
+    component = EntityComponent(_LOGGER, DOMAIN, hass, timedelta(DEFAULT_SCAN_INTERVAL))
 
     with patch(
         "homeassistant.components.decora_wifi.common.DecoraWiFiSession",
@@ -158,24 +177,25 @@ def test_DecoraWifiPlatform_apigetdevices_commfailed():
         "homeassistant.components.decora_wifi.common.ResidentialAccount",
         side_effect=ValueError,
     ), patch(
-        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform.api_logout"
+        "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_logout"
     ):
         # Setup Object
         try:
             instance = DecoraWifiPlatform(USERNAME, PASSWORD)
+            await component.async_add_entities([instance], False)
             instance.setup()
-        except DecoraWifiCommFailed as ex:
+        except CommFailed as ex:
             exception = ex
-        assert isinstance(exception, DecoraWifiCommFailed)
+        assert isinstance(exception, CommFailed)
 
 
-def test_DecoraWifiPlatform_apilogout_commfailed():
+async def test_DecoraWifiPlatform_apilogout_commfailed(hass):
     """Check DecoraWifiPlatform comm failure during deletion."""
     exception = None
 
     with patch(
-        "homeassistant.components.decora_wifi.common.DecoraWiFiSession.login",
-        return_value=True,
+        "homeassistant.components.decora_wifi.common.DecoraWiFiSession",
+        side_effect=FakeDecoraWiFiSession,
     ), patch(
         "homeassistant.components.decora_wifi.common.DecoraWifiPlatform._api_get_devices"
     ), patch(
@@ -186,18 +206,20 @@ def test_DecoraWifiPlatform_apilogout_commfailed():
         try:
             instance = DecoraWifiPlatform(USERNAME, PASSWORD)
             instance.setup()
-            instance.api_logout()
-        except DecoraWifiCommFailed as ex:
+            instance.teardown()
+        except CommFailed as ex:
             exception = ex
-        assert isinstance(exception, DecoraWifiCommFailed)
+        assert isinstance(exception, CommFailed)
         mock_person_logout.assert_called_once()
 
 
-def test_DecoraWifiEntity_init():
+async def test_DecoraWifiEntity_init(hass):
     """Check DecoraWifiEntity initialization."""
     session = FakeDecoraWiFiSession()
     switch = FakeDecoraWiFiIotSwitch(session)
 
     entity = DecoraWifiEntity(switch)
+
     assert entity._switch == switch
-    assert entity._unique_id == switch.mac
+    assert entity.unique_id == switch.mac
+    assert entity.name == SWITCH_NAME
--- tests/components/decora_wifi/test_config_flow.py
+++ tests/components/decora_wifi/test_config_flow.py
@@ -4,17 +4,18 @@ from unittest.mock import patch
 
 from homeassistant import config_entries, data_entry_flow, setup
 from homeassistant.components.decora_wifi.common import (
-    DecoraWifiCommFailed,
-    DecoraWifiLoginFailed,
+    CommFailed,
     DecoraWifiPlatform,
-    decorawifisessions,
+    LoginFailed,
 )
 from homeassistant.components.decora_wifi.const import CONF_TITLE, DOMAIN
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_ID, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
-from tests.components.decora_wifi.common import MockDecoraWifiPlatform
+from tests.components.decora_wifi.common import USER_ID, MockDecoraWifiPlatform
 
+# Test inputs
 USERNAME = "username@home-assisant.com"
 UPDATED_USERNAME = "updated_username@home-assitant.com"
 PASSWORD = "test-password"
@@ -24,14 +25,14 @@ SCAN_INTERVAL = 120
 UPDATED_SCAN_INTERVAL = 60
 
 
-async def test_import_flow(hass):
+async def test_import_flow(hass: HomeAssistant):
     """Check import flow."""
 
     with patch.object(
         DecoraWifiPlatform,
         "async_setup_decora_wifi",
         MockDecoraWifiPlatform.async_setup_decora_wifi,
-    ), patch.dict(decorawifisessions) as mock_decorawifisessions, patch(
+    ), patch(
         "homeassistant.components.decora_wifi.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.decora_wifi.async_setup_entry",
@@ -42,21 +43,22 @@ async def test_import_flow(hass):
             context={"source": config_entries.SOURCE_IMPORT},
             data={CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD},
         )
-        assert isinstance(mock_decorawifisessions[USERNAME], MockDecoraWifiPlatform)
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["title"] == f"{CONF_TITLE} - {USERNAME}"
     assert result["data"] == {
         CONF_USERNAME: USERNAME,
         CONF_PASSWORD: PASSWORD,
+        CONF_ID: USER_ID,
     }
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_user_flow(hass):
+async def test_user_flow(hass: HomeAssistant):
     """Test the user flow."""
+    # Test that the flow with no user input produces a form.
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -64,11 +66,12 @@ async def test_user_flow(hass):
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert not result["errors"]
 
+    # Test that the flow with user input calls setup entry with the appropriate data.
     with patch.object(
         DecoraWifiPlatform,
         "async_setup_decora_wifi",
         MockDecoraWifiPlatform.async_setup_decora_wifi,
-    ), patch.dict(decorawifisessions) as mock_decorawifisessions, patch(
+    ), patch(
         "homeassistant.components.decora_wifi.async_setup", return_value=True
     ) as mock_setup, patch(
         "homeassistant.components.decora_wifi.async_setup_entry",
@@ -79,13 +82,13 @@ async def test_user_flow(hass):
             {CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD},
         )
         await hass.async_block_till_done()
-        assert isinstance(mock_decorawifisessions[USERNAME], MockDecoraWifiPlatform)
 
     assert result2["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result2["title"] == f"{CONF_TITLE} - {USERNAME}"
     assert result2["data"] == {
         CONF_USERNAME: USERNAME,
         CONF_PASSWORD: PASSWORD,
+        CONF_ID: USER_ID,
     }
 
     assert len(mock_setup.mock_calls) == 1
@@ -93,7 +96,7 @@ async def test_user_flow(hass):
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
 
 
-async def test_reauth_flow(hass):
+async def test_reauth_flow(hass: HomeAssistant):
     """Test the reauth flow."""
 
     await setup.async_setup_component(hass, "persistent_notification", {})
@@ -103,13 +106,14 @@ async def test_reauth_flow(hass):
         data={
             CONF_USERNAME: USERNAME,
             CONF_PASSWORD: INCORRECT_PASSWORD,
+            CONF_ID: USER_ID,
         },
     )
     mock_config.add_to_hass(hass)
 
     with patch(
         "homeassistant.components.decora_wifi.config_flow.DecoraWifiPlatform.async_setup_decora_wifi",
-        side_effect=DecoraWifiLoginFailed(),
+        side_effect=LoginFailed(),
     ):
         result = await hass.config_entries.flow.async_init(
             DOMAIN,
@@ -127,7 +131,7 @@ async def test_reauth_flow(hass):
         DecoraWifiPlatform,
         "async_setup_decora_wifi",
         MockDecoraWifiPlatform.async_setup_decora_wifi,
-    ), patch.dict(decorawifisessions) as mock_decorawifisessions:
+    ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
             {
@@ -135,8 +139,7 @@ async def test_reauth_flow(hass):
                 CONF_PASSWORD: UPDATED_PASSWORD,
             },
         )
-        assert isinstance(mock_decorawifisessions[USERNAME], MockDecoraWifiPlatform)
-    await hass.async_block_till_done()
+        await hass.async_block_till_done()
 
     assert mock_config.data.get("username") == USERNAME
     assert mock_config.data.get("password") == UPDATED_PASSWORD
@@ -145,7 +148,7 @@ async def test_reauth_flow(hass):
     assert len(hass.config_entries.async_entries(DOMAIN)) == 1
 
 
-async def test_abort_if_existing_entry(hass):
+async def test_abort_if_existing_entry(hass: HomeAssistant):
     """Check flow abort when an entry already exist."""
     config_entry = MockConfigEntry(
         domain=DOMAIN,
@@ -167,7 +170,7 @@ async def test_abort_if_existing_entry(hass):
     assert result["reason"] == "already_configured"
 
 
-async def test_user_flow_invalid_username(hass):
+async def test_user_flow_invalid_username(hass: HomeAssistant):
     """Test user flow with invalid username."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -178,7 +181,7 @@ async def test_user_flow_invalid_username(hass):
 
     with patch(
         "homeassistant.components.decora_wifi.config_flow.DecoraWifiPlatform.async_setup_decora_wifi",
-        side_effect=DecoraWifiLoginFailed(),
+        side_effect=LoginFailed(),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -190,7 +193,7 @@ async def test_user_flow_invalid_username(hass):
     assert result2["errors"] == {"base": "invalid_auth"}
 
 
-async def test_user_flow_invalid_password(hass):
+async def test_user_flow_invalid_password(hass: HomeAssistant):
     """Test user flow with invalid password."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -201,7 +204,7 @@ async def test_user_flow_invalid_password(hass):
 
     with patch(
         "homeassistant.components.decora_wifi.config_flow.DecoraWifiPlatform.async_setup_decora_wifi",
-        side_effect=DecoraWifiLoginFailed(),
+        side_effect=LoginFailed(),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -213,7 +216,7 @@ async def test_user_flow_invalid_password(hass):
     assert result2["errors"] == {"base": "invalid_auth"}
 
 
-async def test_user_flow_no_internet_connection(hass):
+async def test_user_flow_no_internet_connection(hass: HomeAssistant):
     """Test user flow with no internet connection."""
 
     result = await hass.config_entries.flow.async_init(
@@ -225,7 +228,7 @@ async def test_user_flow_no_internet_connection(hass):
 
     with patch(
         "homeassistant.components.decora_wifi.config_flow.DecoraWifiPlatform.async_setup_decora_wifi",
-        side_effect=DecoraWifiCommFailed(),
+        side_effect=CommFailed(),
     ):
         result2 = await hass.config_entries.flow.async_configure(
             result["flow_id"],
@@ -237,7 +240,7 @@ async def test_user_flow_no_internet_connection(hass):
     assert result2["errors"] == {"base": "cannot_connect"}
 
 
-async def test_reauth_flow_no_internet_connection(hass):
+async def test_reauth_flow_no_internet_connection(hass: HomeAssistant):
     """Test reauth flow with no internet connection."""
 
     await setup.async_setup_component(hass, "persistent_notification", {})
@@ -253,7 +256,7 @@ async def test_reauth_flow_no_internet_connection(hass):
 
     with patch(
         "homeassistant.components.decora_wifi.config_flow.DecoraWifiPlatform.async_setup_decora_wifi",
-        side_effect=DecoraWifiCommFailed(),
+        side_effect=CommFailed(),
     ):
         result2 = await hass.config_entries.flow.async_init(
             DOMAIN,
